36c1a5d4c4f1b98404d79781222c2d5b3db430b4
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2025-11-29 20:56:19 +0300

    Add cannon stats

--- module-3/1/Assets/1/Scripts/Gameplay/World/Cannon/CannonController.cs
+++ module-3/1/Assets/1/Scripts/Gameplay/World/Cannon/CannonController.cs
@@ -1,45 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using _1.Gameplay.World.Cannon;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CannonController : MonoBehaviour
 {
     InputActions _inputActions;
+    float _timer;
     Camera _mainCamera;
-    [SerializeField] float maxYRotation = 50f;
-    [SerializeField] float minYRotation = -50f;
-    [SerializeField] float maxXRotation = 30f;
-    [SerializeField] float minXRotation = 0;
-    [SerializeField] float maxShootForce = 20f;
-
-    [SerializeField] float shootXRotation;
-    // [SerializeField] float 
-    [SerializeField] GameObject cannonBallPrefab;
+    [SerializeField] float _maxYRotation = 50f;
+    [SerializeField] float _minYRotation = -50f;
+    [SerializeField] float _maxXRotation = 30f;
+    [SerializeField] float _minXRotation = 0;
+    [SerializeField] CannonStats _cannonStats;
+    [SerializeField] GameObject _cannonBallPrefab;
 
     public void Bind(InputActions inputActions, Camera mainCamera)
     {
         _inputActions = inputActions;
         _mainCamera = mainCamera;
-        inputActions.Gameplay.Shoot.canceled += OnShootCancelled;
+        inputActions.Gameplay.Shoot.started += OnShootStarted;
     }
-
-    void OnShootCancelled(InputAction.CallbackContext obj)
-    {
-        var ball = Instantiate(cannonBallPrefab, transform.position, Quaternion.identity);
-        var ballRigidBody = ball.GetComponent<Rigidbody>();
-        var shootDirection = Quaternion.Euler(0, shootXRotation, 0) * transform.forward;
-        ballRigidBody.AddForce(shootDirection * maxShootForce, ForceMode.Impulse);
-    }
-
     public void Update()
     {
         if (!_inputActions.Gameplay.enabled) return;
+        
+        if (_timer > 0) _timer -= Time.deltaTime;
+        
         var mousePos = Mouse.current.position.ReadValue();
         var mouseXPos = mousePos.x / Screen.width;
         var mouseYPos = mousePos.y / Screen.height;
-        var cannonXRotation = Mathf.Lerp(minXRotation, maxXRotation, mouseYPos);
-        var cannonYRotation = Mathf.Lerp(minYRotation, maxYRotation, mouseXPos);
+        var cannonXRotation = Mathf.Lerp(_minXRotation, _maxXRotation, mouseYPos);
+        var cannonYRotation = Mathf.Lerp(_minYRotation, _maxYRotation, mouseXPos);
         transform.rotation = Quaternion.Euler(-cannonXRotation, cannonYRotation, transform.eulerAngles.z);
+    }
+    void OnShootStarted(InputAction.CallbackContext obj)
+    {
+        if (_timer > 0) return;
+        _timer = _cannonStats.ShootCoolDown;
+        var ball = Instantiate(_cannonBallPrefab, transform.position, Quaternion.identity);
+        var ballRigidBody = ball.GetComponent<Rigidbody>();
+        ballRigidBody.AddForce(transform.forward * _cannonStats.ProjectileSpeed, ForceMode.Impulse);
     }
 }
