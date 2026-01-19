53b6781cff5c038bea1df70f36619ea2b2b72bde
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2026-01-19 15:07:52 +0300

    Fix game event incorrect time

--- ArenaRunner/Assets/Scripts/Utils/EventBus/GameEvent.cs
+++ ArenaRunner/Assets/Scripts/Utils/EventBus/GameEvent.cs
@@ -6,19 +6,21 @@ namespace Utils.EventBus
     {
         public EventName Name { get; }
         public string Description { get; }
-        public float Time => UnityEngine.Time.time;
+        public float Time { get; }
         public object[] Args { get; }
         
         public GameEvent(EventName name, string description)
         {
             Name = name;
             Description = description;
+            Time = UnityEngine.Time.time;
         }
         public GameEvent(EventName name, string description, params object[] args)
         {
             Name = name;
             Description = description;
             Args = args;
+            Time = UnityEngine.Time.time;
         }
 
         public override string ToString()
