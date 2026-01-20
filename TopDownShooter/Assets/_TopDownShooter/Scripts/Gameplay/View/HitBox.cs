45dd6f1451f6ced937a39b1641f6715a76ee02dc
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2026-01-21 02:32:01 +0300

    Add hit and hurt boxes

---
+++ TopDownShooter/Assets/_TopDownShooter/Scripts/Gameplay/View/HitBox.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _TopDownShooter.Scripts.View
+{
+    public class HitBox : MonoBehaviour
+    {
+        [field: SerializeField] public int Damage { get; set;}
+    }
+}
---
+++ TopDownShooter/Assets/_TopDownShooter/Scripts/Gameplay/View/HurtBox.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace _TopDownShooter.Scripts.View
+{
+    public class HurtBox : MonoBehaviour
+    {
+        [field: SerializeField] public int MaxHealth { get; set; }
+        public int CurrentHealth { get; private set; }
+        
+        public delegate void HitHandler(int damage, int currentHealth);
+        public event HitHandler Hit;
+        public event Action Died;
+        void Awake()
+        {
+            CurrentHealth = MaxHealth;
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            var hitBox = other.GetComponent<HitBox>();
+            if (!hitBox) return;
+            TakeDamage(hitBox.Damage);
+            Debug.Log(CurrentHealth);
+        }
+        public void HealFullHealth() => CurrentHealth = MaxHealth;
+        public void TakeDamage(int damage)
+        {
+            CurrentHealth -= damage;
+            Hit?.Invoke(damage, CurrentHealth);
+            if (CurrentHealth <= 0)
+            {
+                CurrentHealth = 0;
+                Died?.Invoke();
+            }
+            else if (CurrentHealth > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+        }
+    }
+}
