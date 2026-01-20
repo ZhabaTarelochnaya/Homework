3bf4cb66ee7f2b9ab16adc8e21cbc28903701629
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2026-01-20 21:56:41 +0300

    Add state machine

---
+++ TopDownShooter/Assets/_TopDownShooter/Scripts/Utils/StateMachine/FSM.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _TopDownShooter.Scripts.Utils.StateMachine
+{
+    public class FSM<T> where T : Enum 
+    {
+        Dictionary<T, FSMState<T>> _states = new();
+        bool _isInitialized;
+        bool _isActive = true;
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                _isInitialized = _isActive == value ? _isInitialized : false;
+                if (CurrentState != null && _isActive && !value)
+                {
+                    CurrentState.OnExit();
+                }
+                _isActive = value;
+            }
+        }
+        public FSMState<T> CurrentState {get; private set;}
+        public void Tick(float deltaTime)
+        {
+            if (CurrentState == null) Debug.LogError("FSM: CurrentState is null");
+            if (!IsActive) return;
+            if (!_isInitialized)
+            {
+                _isInitialized = true;
+                CurrentState.OnEnter();
+            }
+            
+            CurrentState.Tick(deltaTime);
+            var nextStateKey = CurrentState.GetNextState();
+            if (!nextStateKey.Equals(CurrentState.StateName))
+            {
+                Transition(nextStateKey);
+            }
+        }
+        public void Transition(T stateName)
+        {
+            var newState = _states[stateName];
+            if (newState == null)
+            {
+                Debug.LogError($"State with name {stateName} doesn't exist");
+            }
+            CurrentState.OnExit();
+            CurrentState = newState;
+            CurrentState.OnEnter();
+        }
+        public FSM<T> AddState(FSMState<T> state)
+        {
+            if (CurrentState == null)
+            {
+                CurrentState = state;
+            }
+            _states.Add(state.StateName, state);
+            return this;
+        }
+    }
+}
