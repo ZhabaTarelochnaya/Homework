f32716e10106d9dc406b8e9c95772ef098cd8556
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2026-02-26 01:12:15 +0300

    Add map

--- MultplayerFPS/Assets/_MultiplayerFPS/Scripts/Online/Game/GameRoot.cs
+++ MultplayerFPS/Assets/_MultiplayerFPS/Scripts/Online/Game/GameRoot.cs
@@ -1,8 +1,4 @@
-using System;
-using _MultiplayerFPS.Scripts.Controllers;
-using _MultiplayerFPS.Scripts.Services.InputService;
 using _MultiplayerFPS.Scripts.Services.LoggerService;
-using _MultiplayerFPS.Scripts.Services.Move;
 using _MultiplayerFPS.Scripts.Utils.ServiceLocator;
 using Mirror;
 using UnityEngine;
@@ -19,7 +15,6 @@ namespace _MultiplayerFPS.Scripts
         {
             var hudView = Instantiate(_hudPrefab);
             _hudPresenter = new HUDPresenter(hudView);
-            
             Cursor.lockState = CursorLockMode.Locked;
             _loggerService = ServiceLocator.Current.Get<ILoggerService>();
             _loggerService.Log("Match started");
