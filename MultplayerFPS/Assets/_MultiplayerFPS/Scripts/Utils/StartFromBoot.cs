ddc946f7926f3effd0ca5c319d31db13c0880de4
Yaroslav Allas <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2026-03-06 09:42:20 +0300

    Revert "Remove tool script"
    This reverts commit dcb248caf6c018d30a271af41799ec551c60320c.

---
+++ MultplayerFPS/Assets/_MultiplayerFPS/Scripts/Utils/StartFromBoot.cs
@@ -0,0 +1,20 @@
+using _MultiplayerFPS.Scripts.Utils;
+using _MultiplayerFPS.Scripts.Utils.ExceptionPopUp;
+using _MultiplayerFPS.Scripts.Utils.LoadingScreen;
+using _MultiplayerFPS.Scripts.Utils.ServiceLocator;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _MultiplayerFPS.Scripts.Game.Utils
+{
+    public class StartFromBoot 
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void AutostartGame()
+        {
+            #if UNITY_EDITOR
+                SceneManager.LoadScene("Boot");
+            #endif
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Homework
-
-Start Boot scene to play.  
-wasd - move.  
-space - jump.  
-left mouse button - shoot.  
-h- heal.  
-g - grenade.  
+Repository dedicated to homeworks. Each branch corresponds to a new module.\
+wasd - move.\
+space - jump.\
+left mouse button - shoot.\
+h- heal.\
+g - grenade.\
 tab - leaderboard.
-
